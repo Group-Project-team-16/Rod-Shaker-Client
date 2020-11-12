00c2f30fe12e28e68f83b306890a7eb2d1822ae4
RamaRamesses <maulanaribrahim@outlook.com>
2020-11-12 23:18:23 +0700

    Added main homepage html template

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,16 @@
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+export default {
+}
+</script>
+
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Game.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container" id="game">
+    <div class="flex-container">
+      <div id="player"><img src="../sprites/idle-p1.gif"></div>
+      <div id="playertwo"><img src="../sprites/idle-p2.gif"></div>
+    </div>
+            <!-- <div id="enemy"><img src="sprites/enemy-idle.png"></div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+    .flex-container {
+      display: flex;
+      justify-content: space-around;
+    }
+
+    #game {
+    margin: 0 auto;
+    width: 600px;
+    height: 400px;
+    background-image: url("../sprites/background.gif");
+    background-size: 600px 400px;
+    background-repeat: no-repeat;
+    border: 5px solid grey;
+    border-radius: 15px;
+    }
+    #player {
+    top: -8rem;
+    left: 5rem;
+    position: relative;
+    display: inline;
+    }
+
+    #player img {
+      height: 650px;
+    }
+
+    #playertwo {
+    top: -9.5rem;
+    right: 6rem;
+    position: relative;
+    display: inline;
+    }
+
+    #playertwo img {
+      height: 650px;
+    }
+</style>
